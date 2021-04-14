6c514104d9af57055a0069f26fbef44ad3dd0f0c
Kai <1114366961@qq.com>
2021-04-14 16:11:47 +0800

    webpack 1.html-webpack-plugin 2.webpack-dev-server 3.mode 4.entry path.join(__dirname,'src','xxx')

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin =require('html-webpack-plugin')
+module.exports ={
+  mode: 'development',
+  entry:path.join(__dirname,'src','index.js'),
+  output:{
+    filename:'bundle.js',
+    path:path.join(__dirname,'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),  // 根目录
+    open: true,  // 自动打开浏览器
+  }
+
+ }
